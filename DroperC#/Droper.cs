712dac85645c6a42e3a86114788b61b0b601f1e9
Skully <armandsbf3@hotmail.com>
2015-04-15 01:30:46 +0300

    Extended pro airdrop system

--- DroperC#/Droper.cs
+++ DroperC#/Droper.cs
@@ -9,29 +9,48 @@ namespace Droper
     {
         public class DroperPlane : MonoBehaviour
         {
-            bool dropped = false;
-            public float wsize = global::World.Size;
-            public Vector3 dropPoint;
+            public int dropCountIn = 1;
+            public bool[] dropped;
+            public int dropCountTotal;
+            public Vector3 lookAt;
+            public Vector3 startPos;
+            public Vector3[] dropPoints;
             public BaseEntity plane;
+            public float wsize = global::World.Size;
 
             void Update()
             {
-                if (!dropped && Vector3.Distance(dropPoint, this.transform.position) <= 1f)
+                if (!dropped[dropCountIn] && Vector3.Distance(this.transform.position, dropPoints[dropCountIn]) <= 1f)
                 {
                     BaseEntity baseEntity = GameManager.server.CreateEntity("items/supply_drop", this.transform.position);
                     baseEntity.globalBroadcast = true;
                     baseEntity.Spawn();
-                    dropped = true;
-                    //Logger.LogDebug("DROP SPAWNED");
+                    dropped[dropCountIn] = true;
+                    if (dropped[dropCountTotal])
+                    {
+                        lookAt = startPos;
+                    }
+                    else
+                    {
+                        dropCountIn++;
+                        lookAt = dropPoints[dropCountIn];
+                    }
+                    
                 }
+                else if (!dropped[dropCountIn] && Vector3.Distance(this.transform.position, dropPoints[dropCountIn]) <= 100f && this.transform.rotation != Quaternion.LookRotation(dropPoints[dropCountIn]))
+                {
+                    this.transform.LookAt(dropPoints[dropCountIn], new Vector3(0, this.transform.position.y, 0));
+                }
+                Quaternion rotatePoint = Quaternion.LookRotation(lookAt - this.transform.position);
+                Quaternion slowly = Quaternion.Slerp(this.transform.rotation, rotatePoint, 0.3f * Time.deltaTime);
+                this.transform.rotation = slowly;
                 Vector3 forward = this.transform.position + this.transform.forward;
                 forward.y = this.transform.position.y;
-                this.transform.position = Vector3.MoveTowards(this.transform.position, forward, 100f * Time.deltaTime);
+                this.transform.position = Vector3.MoveTowards(this.transform.position, forward, 50f * Time.deltaTime);
                 plane.TransformChanged();
-                if (this.transform.position.x >= wsize || this.transform.position.z >= wsize || this.transform.position.x <= -wsize || this.transform.position.z <= -wsize)
+                if (dropped[dropCountTotal] && (this.transform.position.x >= wsize || this.transform.position.z >= wsize || this.transform.position.x <= -wsize || this.transform.position.z <= -wsize))
                 {
                     this.SendMessage("KillMessage", 1);
-                    //Logger.LogDebug("PLANE KILLED");
                 }
             }
         }
@@ -45,6 +64,7 @@ namespace Droper
                 ini.AddSetting("Settings", "EventEveryMins", "30");
                 ini.AddSetting("Settings", "PlayersNeeded", "5");
                 ini.AddSetting("Settings", "PlanesInSameTime", "1");
+                ini.AddSetting("Settings", "DropsFromOnePlane", "1");
                 ini.AddSetting("Settings", "BroadcastMsgName", "Droper");
                 ini.AddSetting("Settings", "BroadcastMsgLowPlayers", "AIRDROP WAS CANCELLED, NEED MORE PLAYER");
                 ini.AddSetting("Settings", "BroadcastMsgAirdropIncoming", "AIRDROP CARGO PLANE INCOMING");
@@ -52,23 +72,26 @@ namespace Droper
             }
             return Plugin.GetIni("DroperSettings");
         }
+
         public void On_ServerInit()
         {
             EventSchedule[] eventschedule = UnityEngine.Object.FindObjectsOfType<EventSchedule>();
             foreach (EventSchedule each in eventschedule) each.CancelInvoke("RunSchedule");
-        //}
+        }
 
-        //public void On_PluginInit()
-        //{
-        //    EventSchedule[] eventschedule = UnityEngine.Object.FindObjectsOfType<EventSchedule>();
-        //    foreach (EventSchedule each in eventschedule) each.CancelInvoke("RunSchedule");
+        public void On_PluginInit()
+        {
+            ServerConsoleCommands.Register("airdrop").setCallback(SpawnPlaneCMD);
+            ServerConsoleCommands.Register("plane").setCallback(SpawnPlaneCMD);
+            EventSchedule[] eventschedule = UnityEngine.Object.FindObjectsOfType<EventSchedule>();
+            foreach (EventSchedule each in eventschedule) each.CancelInvoke("RunSchedule");
             ServerConsoleCommands.Register("plane").setCallback("SpawnPlane");
             IniParser ini = DroperIniSettings();
             if (ini.GetSetting("Settings", "Enabled") == "1")
             {
                 int mins = int.Parse(ini.GetSetting("Settings", "EventEveryMins"));
                 int timer = mins * 60000;
-                Plugin.CreateTimer("Drop", timer).Start();
+                //Plugin.CreateTimer("Drop", timer).Start();
             }
         }
 
@@ -80,11 +103,11 @@ namespace Droper
             string sysName = ini.GetSetting("Settings", "BroadcastMsgName");
             if (online >= needed)
             {
+                int drops = int.Parse(ini.GetSetting("Settings", "DropsFromOnePlane"));
                 int planes = int.Parse(ini.GetSetting("Settings", "PlanesInSameTime"));
                 for (int i = 0; i < planes; i++)
                 {
-                    //Logger.LogDebug("SPAWNING PLANE");
-                    SpawnPlane();
+                    SpawnPlane(drops);
                     i = i + 1;
                 }
                 string message = ini.GetSetting("Settings", "BroadcastMsgAirdropIncoming");
@@ -97,37 +120,30 @@ namespace Droper
             }
         }
 
-        public void SpawnPlane()
+        public void SpawnPlaneCMD(string[] cmd)
         {
-            bool grounded = false;
+            SpawnPlane(1);
+        }
+
+        public void SpawnPlane(int drops)
+        {
             float worldsize = global::World.Size;
             float y = global::World.HighestPoint.y + 500f;
-            Vector3 startingpos;
-            Vector3 droppingpoint;
-            float x = 50f;
-            float z = 50f;
-            while (!grounded)
-            {
-                RaycastHit hit;
-                x = UnityEngine.Random.Range(-worldsize, worldsize);
-                z = UnityEngine.Random.Range(-worldsize, worldsize);
-                var origin = new Vector3(x, global::World.HighestPoint.y + 1f, z);
-                if (Physics.Raycast(origin, Vector3.down, out hit, global::World.HighestPoint.y + 101f, 1 << 23))
-                {
-                    if (hit.point.y > 0f) grounded = true;
-                    //else Logger.LogDebug("REGENERATING DROP POINT");
-                }
-            }
-            droppingpoint.x = x;
-            droppingpoint.y = y;
-            droppingpoint.z = z;
+            Vector3 startingpos = default(Vector3);
             float pos = UnityEngine.Random.Range(-(worldsize / 3), worldsize / 3);
             int rand = UnityEngine.Random.Range(1, 100);
             int rand2 = UnityEngine.Random.Range(1, 100);
             if (rand >= 50)
             {
                 startingpos.x = pos;
-                if (rand2 <= 50) startingpos.z = worldsize; else startingpos.z = -(worldsize);
+                if (rand2 <= 50)
+                {
+                    startingpos.z = worldsize;
+                }
+                else
+                {
+                    startingpos.z = -(worldsize);
+                }
             }
             else
             {
@@ -135,16 +151,47 @@ namespace Droper
                 if (rand2 <= 50) startingpos.x = worldsize; else startingpos.x = -(worldsize);
             }
             startingpos.y = y;
-            BaseEntity baseEntity = GameManager.server.CreateEntity("events/cargo_plane", startingpos, Quaternion.LookRotation(droppingpoint));
+            Vector3[] droppingpoint = new Vector3[drops + 1];
+            for (int i = 1; i <= drops; i++)
+            {
+                bool grounded = false;
+                while (!grounded)
+                {
+                    RaycastHit hit;
+                    float x = UnityEngine.Random.Range(-worldsize, worldsize);
+                    float z = UnityEngine.Random.Range(-worldsize, worldsize);
+                    var origin = new Vector3(x, global::World.HighestPoint.y + 1f, z);
+                    if (Physics.Raycast(origin, Vector3.down, out hit, global::World.HighestPoint.y + 101f, 1 << 23))
+                    {
+                        if (hit.point.y > 0f)
+                        {
+                            grounded = true;
+                            droppingpoint[i].x = x;
+                            droppingpoint[i].y = y;
+                            droppingpoint[i].z = z;
+                        }
+                    }
+                }
+            }
+            BaseEntity baseEntity = GameManager.server.CreateEntity("events/cargo_plane", startingpos, Quaternion.LookRotation(droppingpoint[1]));
             baseEntity.Spawn();
             CargoPlane[] cp = baseEntity.GetComponents<CargoPlane>();
-            foreach (CargoPlane each in cp) each.enabled = false;
+            foreach (CargoPlane each in cp)
+            {
+                each.enabled = false;
+            }
             baseEntity.transform.position = startingpos;
-            baseEntity.transform.LookAt(droppingpoint, new Vector3(0,y,0));
+            baseEntity.transform.LookAt(droppingpoint[1], new Vector3(0,y,0));
             baseEntity.TransformChanged();
             DroperPlane classy = baseEntity.gameObject.AddComponent<DroperPlane>();
-            classy.dropPoint = droppingpoint;
+            classy.dropPoints = new Vector3[drops + 1];
+            classy.dropped = new bool[drops + 1];
+            classy.dropPoints = droppingpoint;
+            classy.lookAt = droppingpoint[1];
+            classy.dropCountTotal = drops;
+            classy.startPos = startingpos;
             classy.plane = baseEntity;
+            Logger.Log("[EVENT] AIRDROP CARGO PLANE IN SERVER");
         }
     }
 }
